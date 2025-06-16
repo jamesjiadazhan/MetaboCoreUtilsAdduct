c79276ab6e6c6d205112823c3d375e9d3e65aaa1
James Jiada Zhan <jzha832@emory.edu>
2025-06-16 12:17:07 -0400

    Update mass2mz.R

--- R/mass2mz.R
+++ R/mass2mz.R
@@ -1,15 +1,9 @@
-mass2mz <- function(x, adduct = "M+H", custom_adduct = NULL) {
+mass2mz <- function(x, adduct = "M+H") {
 
-    # if the user provides a custom adduct, use that
-    if (!is.null(custom_adduct)) {
-        adduct_definition = custom_adduct
-    } 
-    # otherwise, use the adduct definition in the package
-    else {
-        data(adduct_definition)
-        adduct_definition = adduct_definition %>% 
-            filter(name %in% adduct)
-    }
+    #  use the adduct definition in the package
+    data(adduct_definition)
+    adduct_definition = adduct_definition %>% 
+        filter(name %in% adduct)
 
     # calculate the adduct mass in a wide format
     res <- outer(x, adduct_definition$mass_multi) +
